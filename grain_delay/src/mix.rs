b8914e81c96cf76632dc71484772bf6dee007da6
Dave Mollen <dave.mollen@infi.nl>
2023-04-15 16:01:04 +0200

    Update mix

--- grain_delay/src/mix.rs
+++ grain_delay/src/mix.rs
@@ -1,13 +1,11 @@
-use std::f32::{self, consts::FRAC_1_SQRT_2};
+use std::f32::consts::FRAC_PI_2;
 
 pub struct Mix;
 
 impl Mix {
   pub fn run(dry: f32, wet: (f32, f32), mix: f32) -> (f32, f32) {
-    let twopi = f32::consts::PI * 2.;
-    let phase = mix * 0.25;
-    let dry_gain = (phase * twopi).cos() * FRAC_1_SQRT_2;
-    let wet_gain = ((phase + 0.75) * twopi).cos();
+    let dry_gain = (mix * FRAC_PI_2).cos();
+    let wet_gain = (mix * FRAC_PI_2).sin();
     let dry_out = dry * dry_gain;
     (dry_out + wet.0 * wet_gain, dry_out + wet.1 * wet_gain)
   }
@@ -19,8 +17,8 @@ mod tests {
 
   fn assert_approximately_eq(left: f32, right: f32) {
     assert_eq!(
-      (left * 1000.).round() / 1000.,
-      (right * 1000.).round() / 1000.
+      (left * 1000.).floor() / 1000.,
+      (right * 1000.).floor() / 1000.
     )
   }
 
