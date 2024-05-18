42a129ef0641ad1c3e6e257d1e80d8cd9a23da7b
Dave Mollen <dave.mollen@infi.nl>
2024-05-18 22:11:40 +0200

    Fix filter

--- grain_delay/src/one_pole_filter_stereo.rs
+++ grain_delay/src/one_pole_filter_stereo.rs
@@ -9,7 +9,7 @@ pub struct OnePoleFilterStereo {
 impl OnePoleFilterStereo {
   pub fn new(sample_rate: f32) -> Self {
     Self {
-      t: sample_rate.recip() * TAU,
+      t: sample_rate.recip() * -TAU,
       z: (0., 0.),
     }
   }
@@ -17,7 +17,6 @@ impl OnePoleFilterStereo {
   pub fn process(&mut self, input: (f32, f32), cutoff_freq: f32) -> (f32, f32) {
     let b1 = (cutoff_freq * self.t).fast_exp();
     let a0 = 1.0 - b1;
-
     self.z = (input.0 * a0 + self.z.0 * b1, input.1 * a0 + self.z.1 * b1);
     self.z
   }
