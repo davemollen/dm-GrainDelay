7759bab453b8af1fd60ee6a857d86ad5a220bbdc
Dave Mollen <dave.mollen@infi.nl>
2024-05-30 16:54:07 +0200

    Use efficient and accurate one pole filter

--- grain_delay/src/one_pole_filter_stereo.rs
+++ grain_delay/src/one_pole_filter_stereo.rs
@@ -1,9 +1,10 @@
-use crate::shared::float_ext::FloatExt;
 use std::f32::consts::TAU;
 
 pub struct OnePoleFilterStereo {
   t: f32,
   z: (f32, f32),
+  prev_freq: f32,
+  b1: f32,
 }
 
 impl OnePoleFilterStereo {
@@ -11,13 +12,22 @@ impl OnePoleFilterStereo {
     Self {
       t: sample_rate.recip() * -TAU,
       z: (0., 0.),
+      prev_freq: 0.,
+      b1: 0.,
     }
   }
 
-  pub fn process(&mut self, input: (f32, f32), cutoff_freq: f32) -> (f32, f32) {
-    let b1 = (cutoff_freq * self.t).fast_exp();
-    let a0 = 1.0 - b1;
-    self.z = (input.0 * a0 + self.z.0 * b1, input.1 * a0 + self.z.1 * b1);
+  pub fn process(&mut self, input: (f32, f32), freq: f32) -> (f32, f32) {
+    if freq != self.prev_freq {
+      self.b1 = (freq * self.t).exp();
+      self.prev_freq = freq;
+    }
+
+    let a0 = 1.0 - self.b1;
+    self.z = (
+      input.0 * a0 + self.z.0 * self.b1,
+      input.1 * a0 + self.z.1 * self.b1,
+    );
     self.z
   }
 }
