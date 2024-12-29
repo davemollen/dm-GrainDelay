1cdecb2a739cae20e4499df070de6eef4c8eb35b
davemollen <davemollen@gmail.com>
2024-12-29 12:52:37 +0100

    Fix drift

--- grain_delay/src/grains/grain.rs
+++ grain_delay/src/grains/grain.rs
@@ -18,7 +18,7 @@ pub struct Grain {
   time_ramp: Ramp,
   phase_offset: f32,
   delta: Delta,
-  time_multiplier: f32,
+  drift: f32,
   is_reversed: bool,
 }
 
@@ -32,7 +32,7 @@ impl Grain {
       time_ramp: Ramp::new(sample_rate),
       phase_offset: (VOICES as f32).recip() * index as f32,
       delta: Delta::new(),
-      time_multiplier: 1.,
+      drift: 1.,
       is_reversed: false,
     }
   }
@@ -49,9 +49,9 @@ impl Grain {
     pan: f32,
   ) -> (f32, f32) {
     let phase = Self::wrap(phasor + self.phase_offset);
-    let trigger = self.delta.process(phase).abs() > 0.5;
+    let trigger = self.delta.process(phase) < 0.;
     if trigger {
-      self.set_grain_params(freq, speed, spray, drift, reverse, pan);
+      self.set_grain_params(freq, spray, drift, reverse, pan);
     }
 
     let (ramp, time) = self.get_ramp_and_time(speed);
@@ -61,22 +61,14 @@ impl Grain {
     grains_out.pan(self.pan)
   }
 
-  fn set_grain_params(
-    &mut self,
-    freq: f32,
-    speed: f32,
-    spray: f32,
-    drift: f32,
-    reverse: f32,
-    pan: f32,
-  ) {
+  fn set_grain_params(&mut self, freq: f32, spray: f32, drift: f32, reverse: f32, pan: f32) {
     self.freq = freq;
     self.time_ramp.start();
     self.start_position = fastrand::f32() * spray;
     self.pan = (fastrand::f32() * pan * 2. - pan) * 50.;
     self.is_reversed = fastrand::f32() <= reverse;
     self.window_size = freq.recip() * 1000.;
-    self.set_time_multiplier(speed, drift);
+    self.set_drift(drift);
   }
 
   fn get_speed_for_delay_line(&self, speed: f32) -> f32 {
@@ -87,30 +79,17 @@ impl Grain {
     }
   }
 
-  fn get_drift(&mut self, drift: f32) -> f32 {
+  fn set_drift(&mut self, drift: f32) {
     let random_pitch = fastrand::f32() * drift * 2. - drift;
-    2_f32.powf(random_pitch / 12.)
-  }
-
-  fn set_time_multiplier(&mut self, speed: f32, drift: f32) {
-    let drift = self.get_drift(drift);
-    self.time_multiplier = self.get_speed_for_delay_line(speed * drift).abs();
+    self.drift = 2_f32.powf(random_pitch / 12.);
   }
 
   fn get_ramp_and_time(&mut self, speed: f32) -> (f32, f32) {
-    if self.time_multiplier == 0. {
-      let ramp = self.time_ramp.process(self.freq);
-      (
-        ramp,
-        Self::wrap(ramp * self.get_speed_for_delay_line(speed)) * self.window_size,
-      )
-    } else {
-      let ramp_freq = self.get_speed_for_delay_line(speed) * self.freq;
-      let ramp = self
-        .time_ramp
-        .process(ramp_freq * self.time_multiplier.recip());
-      (ramp, ramp * self.time_multiplier * self.window_size)
-    }
+    let speed = self.get_speed_for_delay_line(speed * self.drift);
+    let ramp = self
+      .time_ramp
+      .process(if speed >= 0. { self.freq } else { -self.freq });
+    (ramp, ramp * speed.abs() * self.window_size)
   }
 
   fn wrap(input: f32) -> f32 {
