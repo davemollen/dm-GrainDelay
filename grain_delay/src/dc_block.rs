4516c04cf72faaef9687228a7079a1d62c5845a2
davemollen <davemollen@gmail.com>
2025-01-25 16:48:24 +0100

    More efficient dc block

--- grain_delay/src/dc_block.rs
+++ grain_delay/src/dc_block.rs
@@ -1,5 +1,5 @@
 pub struct DcBlock {
-  sample_rate: f32,
+  coeff: f32,
   xm1: f32,
   ym1: f32,
 }
@@ -7,15 +7,14 @@ pub struct DcBlock {
 impl DcBlock {
   pub fn new(sample_rate: f32) -> Self {
     Self {
-      sample_rate,
+      coeff: 1. - (220.5 / sample_rate),
       xm1: 0.,
       ym1: 0.,
     }
   }
 
   pub fn process(&mut self, x: f32) -> f32 {
-    let coeff = 1. - (220.5 / self.sample_rate);
-    let y = x - self.xm1 + coeff * self.ym1;
+    let y = x - self.xm1 + self.coeff * self.ym1;
     self.xm1 = x;
     self.ym1 = y;
     y
