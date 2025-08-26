7269f18b706b39bbc677e9b3804847f50b060f96
Dave Mollen <davemollen@gmail.com>
2025-08-26 11:13:13 +0200

    Fix linear smoother and overshoot on all smoothers

--- grain_delay/src/params/smooth.rs
+++ grain_delay/src/params/smooth.rs
@@ -15,7 +15,8 @@ pub struct LinearSmooth {
   target: f32,
   factor: f32,
   step_size: f32,
-  is_active: bool,
+  steps: usize,
+  step_counter: usize,
 }
 
 impl LinearSmooth {
@@ -25,7 +26,8 @@ impl LinearSmooth {
       target: 0.,
       factor: sample_rate.recip() * frequency,
       step_size: 0.,
-      is_active: false,
+      steps: (sample_rate / frequency) as usize,
+      step_counter: 0,
     }
   }
 }
@@ -33,14 +35,14 @@ impl LinearSmooth {
 impl Smoother for LinearSmooth {
   fn reset(&mut self, target: f32) {
     self.current = target;
-    self.is_active = false;
+    self.step_counter = 0;
   }
 
   fn set_target(&mut self, target: f32) {
-    self.target = target;
-    self.is_active = self.current != self.target;
-    if self.is_active {
+    if target != self.target {
       self.step_size = (target - self.current) * self.factor;
+      self.target = target;
+      self.step_counter = self.steps;
     }
   }
 
@@ -49,16 +51,13 @@ impl Smoother for LinearSmooth {
   }
 
   fn next(&mut self) -> f32 {
-    if !self.is_active {
-      return self.current;
-    }
-    if (self.current - self.target).abs() <= f32::EPSILON {
+    if self.step_counter > 0 {
+      self.current += self.step_size;
+      self.step_counter -= 1;
+    } else {
       self.current = self.target;
-      self.is_active = false;
-      return self.target;
     }
-    self.current += self.step_size;
-    return self.current;
+    self.current
   }
 }
 
@@ -96,17 +95,15 @@ impl Smoother for ExponentialSmooth {
   }
 
   fn next(&mut self) -> f32 {
-    if !self.is_active {
-      return self.current;
+    if self.is_active {
+      let a0 = 1.0 - self.b1;
+      self.current = self.target * a0 + self.current * self.b1;
+      if (self.current - self.target).abs() <= f32::EPSILON {
+        self.current = self.target;
+        self.is_active = false;
+      }
     }
-    if (self.current - self.target).abs() <= f32::EPSILON {
-      self.current = self.target;
-      self.is_active = false;
-      return self.target;
-    }
-    let a0 = 1.0 - self.b1;
-    self.current = self.target * a0 + self.current * self.b1;
-    return self.current;
+    self.current
   }
 }
 
@@ -144,17 +141,41 @@ impl Smoother for LogarithmicSmooth {
   }
 
   fn next(&mut self) -> f32 {
-    if !self.is_active {
-      return self.current;
+    if self.is_active {
+      let difference = self.target - self.current;
+      let ad = 0.693147 * self.factor;
+      self.current += difference * ad;
+      if (self.current - self.target).abs() <= f32::EPSILON {
+        self.current = self.target;
+        self.is_active = false;
+      }
     }
-    if (self.current - self.target).abs() <= f32::EPSILON {
-      self.current = self.target;
-      self.is_active = false;
-      return self.target;
-    }
-    let difference = self.target - self.current;
-    let ad = 0.693147 * self.factor;
-    self.current += difference * ad;
-    return self.current;
+    self.current
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::params::{smooth::LinearSmooth, Smoother};
+
+  #[test]
+  fn should_smooth_linearly() {
+    let mut linear_smooth = LinearSmooth::new(5., 1.);
+    linear_smooth.set_target(1.);
+    assert_eq!(linear_smooth.next(), 0.2);
+    linear_smooth.set_target(1.);
+    assert_eq!(linear_smooth.next(), 0.4);
+    assert_eq!(linear_smooth.next(), 0.6);
+    assert_eq!(linear_smooth.next(), 0.8);
+    assert_eq!(linear_smooth.next(), 1.0);
+    assert_eq!(linear_smooth.next(), 1.0);
+    linear_smooth.set_target(0.);
+    assert_eq!(linear_smooth.next(), 0.8);
+    linear_smooth.set_target(0.);
+    assert_eq!(linear_smooth.next(), 0.6);
+    assert_eq!(linear_smooth.next(), 0.40000004);
+    assert_eq!(linear_smooth.next(), 0.20000003);
+    assert_eq!(linear_smooth.next(), 2.9802322e-8);
+    assert_eq!(linear_smooth.next(), 0.0);
   }
 }
